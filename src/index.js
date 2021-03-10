65c936a6bf12077ed2893865e6715c8c0afc03cd
github-gtfockout <marsik100041@mail.ru>
2021-03-10 08:57:27 +0300

    feat: added brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+  let leftPart = [];
+  let rightPart = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    leftPart.push(bracketsConfig[i][0]);
+    rightPart.push(bracketsConfig[i][1]);
+  }
+  for (let i = 0; i < str.length; i++) {
+    let first = rightPart.indexOf(str[i]);
+    let second = leftPart.indexOf(str[i]);
+    if (first !== -1) {
+      let top = result[result.length - 1];
+      if (first === second && (top != str[i] || result.length === 0)) result.push(str[i]);
+      else if (leftPart.indexOf(top) === first) result.pop();
+      else return false
+    } 
+    else result.push(str[i]);
+  }
+  return result.length === 0;
 }
